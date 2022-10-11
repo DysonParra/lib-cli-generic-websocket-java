6f09a58a284eba45a921e66ed5e3730f245f8943
Dyson <dysontilano@gmail.com>
2022-10-11 11:51:12 -0500

    Updated license of '*.java' files

--- src/main/java/com/rtc/dummy/websocket/tcp/generic/server/ServerScheduledMessage.java
+++ src/main/java/com/rtc/dummy/websocket/tcp/generic/server/ServerScheduledMessage.java
@@ -1,13 +1,13 @@
 /*
- * @fileoverview {FileName} se encarga de realizar tareas especificas.
+ * @fileoverview {ServerScheduledMessage} se encarga de realizar tareas especificas.
  *
  * @version             1.0
  *
  * @author              Dyson Arley Parra Tilano <dysontilano@gmail.com>
  * Copyright (C) Dyson Parra
  *
- * @History v1.0 --- La implementacion de {FileName} fue realizada el 31/07/2022.
- * @Dev - La primera version de {FileName} fue escrita por Dyson A. Parra T.
+ * @History v1.0 --- La implementacion de {ServerScheduledMessage} fue realizada el 31/07/2022.
+ * @Dev - La primera version de {ServerScheduledMessage} fue escrita por Dyson A. Parra T.
  */
 package com.rtc.dummy.websocket.tcp.generic.server;
 
